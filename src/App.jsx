3a4cd688d1abb55e277566978f431779fc597ab4
DungNg <87799163+nminhdung@users.noreply.github.com>
2024-01-18 11:21:14 +0700

    create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+
+
 function App() {
   return (
     <>
-     <h1 className='text-3xl text-red-500'>hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/sign-in' element={<Signin />} />
+          <Route path='/sign-up' element={<Signup />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
