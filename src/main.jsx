e2f8f8af77871bb4c550a804dbe798b955f06472
DungNg <87799163+nminhdung@users.noreply.github.com>
2024-01-31 09:41:15 +0700

    complete dark-mode

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,11 @@ import { persistor, store } from './redux/store';
 
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
+import ThemeProvider from './components/ThemeProvider.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider><App /></ThemeProvider>
     </Provider>
   </React.StrictMode>,
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import { persistStore } from 'redux-persist';
 import userReducer from './user/userSlice';
+import themeReducer from './theme/themeSlice';
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
@@ -13,9 +14,11 @@ const userConfig = {
   whiteList: ['currentUser']
 };
 
+
 export const store = configureStore({
   reducer: {
-    user: persistReducer(userConfig, userReducer)
+    user: persistReducer(userConfig, userReducer),
+    theme: persistReducer(persistConfig, themeReducer)
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware({
