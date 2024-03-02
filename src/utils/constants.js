08bcfe1f7c75d566bd1f0f567c17d5503cc82f4e
DungNg <87799163+nminhdung@users.noreply.github.com>
2024-03-02 08:55:01 +0700

    deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,8 @@
-let apiRoot = 'http://localhost:3000';
-
-
+let apiRoot = '';
+if (process.env.BUILD_MODE === 'dev') {
+    apiRoot = 'http://localhost:3000'
+}
+if (process.env.BUILD_MODE === 'production') {
+    apiRoot = 'https://blog-api-bvh2.onrender.com'
+}
 export const API_ROOT = apiRoot;
